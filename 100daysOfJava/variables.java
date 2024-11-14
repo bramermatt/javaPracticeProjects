e89cf75fe4b3ab4ee46645e250ce4e7ac1bdb351
Matthew T. Bramer <mbramer@mercer-trans.com>
2024-11-14 11:26:48 -0500

    java day 2 or 100 - variables

---
+++ 100daysOfJava/variables.java
@@ -0,0 +1,53 @@
+public class variables {
+    public static void main(String[] args) {
+        int myInt = 4;
+        double myDouble = 15.15;
+        String myString = "working";
+
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("String: " + myString);
+
+    /*
+    **  100 days of code DAY 2
+    **  (https://www.100daysofcode.io/learn/java/variables-and-data-types)
+    **
+    **  1. Declare and initialize variables for various data types including integer (int), floating-point number (double), and string (String). Then, print each variable to the console. 
+    **
+    **  Reading List:
+    **  1. Learn the terms: initialization, declaration and assignment of variables
+    **     source (https://learn-it-university.com/understanding-java-demystifying-initialization-declaration-and-assignment/)
+    **
+    **      Initialization: gives a variable it's initial value. Assigning a variable a starting value. For objects, this means setting them to an initial state (often null if they are not yet pointing to an object), while primitive data types have default values like 0 for integers and false for booleans. Initialization represents the very first assignment to a variable, and it can occur alongside declaration or at a later stage in the program. Proper initialization is vital to ensure variables start with the correct values.
+    **
+    **      Declaration: declaration refers to the statement where you define the type and name of a variable. It essentially tells the compiler the data type the variable will hold and what it will be called. One key point to note is that a variable can only be declared one within a specific scope. Declarations help prevent errors by ensuring that you don't mistakenly assign incompatible values to variables. Before using a variable, you must declare it. 
+    **
+    **      Assignment: assignment involves replacing the current value of a variable with a new value. This is the process of giving a variable a different value AFTER it has already been declared and initialized. By assigning, you update the content of the variable throughout the program's execution. Understanding when and how to perform assignments accurately is key to manipulating data and ensuring your program's logic functions correctly. 
+    **
+    **  2. Learn the difference between Primitive and Non-Primitive data types
+    **      source (https://www.w3schools.com/java/java_data_types_non-prim.asp)
+    **
+    **      Primitive Data Types: predefined and built into the language. Start with a lowercase letter (like int). Primitive types always hold a value.
+    **
+    **      Non-Primitive Data Types: created by the programmer (except for String). Typically start with an uppercase letter (like String). Non-primitive types can be (null).
+    **
+    **
+    **  3. Default values and sizes of primitive data types
+    **      source (https://cs-fundamentals.com/java-programming/java-primitive-data-types)
+    **  
+    **      Primitive Data Type Values and Sizes: 
+    **      byte: 1 byte - default value: 0
+    **      short: 2 bytes - default value: 0
+    **      int: 4 bytes - default value: 0
+    **      long: 8 bytes - default value: 0
+    **      float: 4 bytes - default value: 0.0f
+    **      double: 8 bytes - default value: 0.0d
+    **      char: 2 byte - default value: '\u0000'
+    **      boolean: not precisely defined, true or false - default value: false
+    **      String: null
+    **
+    **      
+    */
+
+    }
+}
