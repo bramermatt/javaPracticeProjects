875427c40aa09c16b86fffdbe3fdcb29084f04a9
Matthew T. Bramer <mbramer@mercer-trans.com>
2024-11-14 09:36:49 -0500

    java day 1 of 100

---
+++ 100daysOfJava/helloWorld.java
@@ -0,0 +1,40 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+/*  
+**  100 days of code DAY 1 
+**  (https://www.100daysofcode.io/learn/java/hello-world)
+**
+**  Day 1.2 says add a single line or multi line comments to code.
+** 
+**  Reading List 1
+**  Now to read the difference between Java and Python
+**      source (https://www.coursera.org/articles/python-vs-java)
+**      second (https://realpython.com/java-vs-python/)
+**
+**      Java: is used for web dev, big data, and android dev. Enterprise-level web apps and microservices. It is fast and efficient. Platform independent - it can be run on multiple types of computers: Windows, Unix, and Linux (as long as it has the Java Virtual Machine Platform). Object oriented. Multi-threaded. Secure without using pointers, uses byte-code. Java uses a large amount of memory compared to Python. Java manages memory through garbage collection - that clears it from memory but during the process other threads have to stop while the garbage collector works, sometimes impacting overall performance. 
+**
+**      Python: SIMPLE SYNTAX, that's what I've always heard and seen. IoT, data science, and machine learning applications. It is an interpreted language, meaning the program gets run through interpreters on a line-by-line basis for each command's execution. Python is also object oriented, but also also for functional and reflective programming (not sure what reflective programming is atm). Python is used for Back-end Dev and App Dev, data science and machine learning. Platform independent as well, just need an interpreter designed for that platform. Fast and friendly syntax for development, it uses asynchronous code to tackle situations and challenges faster because each unit of code runs separately. Extensive libraries to support common tasks and commands. It also contains code that can be used for many different purposes, ranging from generating documentation to unit testing to CGI (Computer-Generated Imagery). Python is easy and fast to learn, but it is slower to execute because it's a dynamically typed, interpreted language, executed line-by-line. It executes one thread at a time, it isn't mobile native, and it also uses a large amount of memory. 
+**
+**
+**  Reading List 2 
+**  What is a Java Compiler and how does it work?
+**      source (https://www.theserverside.com/definition/Java-compiler) 
+**      
+**      Java Compiler: a program that takes the text file work of a developer and compiles it into a platform independent Java file. Java compilers include the Java Programming Language Compiler (javac), the GNU Compiler for Java (GCJ), the Eclipse Compiler for Java (ECJ), and Jikes. Programmers typically write language statements in a given programming language one line at a time using a code editor or an integrated development environment (IDE). The resulting file contains what are called the source statements. The programmer then runs a compiler for the appropriate language, specifying the name of the file that contains the source statements. 
+**
+**  Reading List 3
+**  Difference between Compiler and Interpreter
+**      source (https://www.programiz.com/article/difference-compiler-interpreter)
+**
+**      Interpreter Vs Compiler: we generally write a computer program using a high-level language - this is called source code. A computer, however, doesn't understand the high-level language. It only understands the program written in 0's and 1's in binary, called the machine code. To convert source code into machine code, we use either a compiler or an interpreter. 
+**
+**      Interpreter: translates program one statement at a time. Usually take less amount of time to analyze the source code. However, the overall execution time is comparatively slower than compilers. No Object Code is generated, hence are memory efficient. Programming languages like Javascript, Python, and Ruby use interpreters. 
+**
+**      Compiler: scans the entire program and translates it as a whole into machine code. Compilers usually take a large amount of time to analyze the source code. The overall execution time is comparatively faster than interpreters. Compilers generate object code which further requires linking, hence requires more memory. Programming langues like C, C++, and Java use compilers. 
+*/
+
+
+    }
+}
