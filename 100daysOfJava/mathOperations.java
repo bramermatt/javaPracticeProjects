cb46080f704ab964c4880380f4fafc2aa01210b1
Matthew T. Bramer <mbramer@mercer-trans.com>
2024-11-22 15:19:14 -0500

    start of day 6

---
+++ 100daysOfJava/mathOperations.java
@@ -0,0 +1,19 @@
+public class mathOperations {
+    public static void main(String[] args) {
+        
+        
+
+    /*
+    **  Day 7 - Math Operations (https://www.100daysofcode.io/learn/java/math-operations)
+        Write a program that performs the following tasks using Math class:
+
+        Maximum of 2 numbers
+        Minimum of 2 numbers
+        Square root of a number
+        Floor and Ceil of a float
+        Abs value of a double
+
+    **
+    */
+    }
+}
